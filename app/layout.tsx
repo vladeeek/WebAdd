366396d35b5d0ccdcd095940f6bcf1345eb7fed6
Vladislav Yakimenko <ya_kimenko@ukr.net>
2023-11-24 23:39:20 +0200

    update readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ import './globals.css'
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Spotify Clone',
-  description: 'Spotify Clone',
+  title: 'YakimenkoAdd',
+  description: 'YakimenkoAdd',
 }
 
 export const revalidate = 0;
